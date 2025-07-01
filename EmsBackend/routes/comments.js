af216940280c4f6de2d7d0143587928d48b299d8
Raifulalam <raifulalam0123@gmail.com>
2025-07-01 08:33:56 +0530

    added backend

--- EmsBackend/routes/comments.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const { addComment } = require('../controllers/commentController');
-
-router.post('/:postId', auth, addComment);
-
-module.exports = router;
